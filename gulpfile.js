65c495d071916d6d983cde0151afb5eddddd7bfb
unknown <you@example.com>
2019-05-06 21:17:16 +0800

    第一天

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 const gulp = require("gulp");
 const connect = require("gulp-connect");
-const babel = require("gulp-babel");
-const uglify = require("gulp-uglify");
-const sass = require("gulp-sass");
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
 
 gulp.task('connect',function(){
     connect.server({
@@ -14,19 +15,31 @@ gulp.task('connect',function(){
 
 gulp.task("html",()=>{
     return gulp.src("./src/html/*.html")
-        .pipe(gulp.dest("./dist/"))
+        .pipe(gulp.dest("./dist/html"))
         .pipe(connect.reload());
 })
 
 gulp.task("js",()=>{
     return gulp.src("./src/scripts/*.js")
+        .pipe(babel({
+            presets : ['@babel/env']
+        }))
+        .pipe(uglify())
         .pipe(gulp.dest("./dist/scripts"))
         .pipe(connect.reload());
 })
 
+gulp.task('sass', function () {
+    return gulp.src('./src/sass/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./dist/styles'))
+      .pipe(connect.reload());
+  });
+
 gulp.task("watch",()=>{
-    gulp.watch("./src/html/*.html",["html"])
-    gulp.watch("./src/scripts/*.js",["js"])
+    gulp.watch("./src/html/*.html",["html"]);
+    gulp.watch("./src/scripts/*.js",['js']);
+    gulp.watch('./src/sass/*.scss',['sass']);
 
 })
 
